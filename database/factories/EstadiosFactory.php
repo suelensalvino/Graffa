7f750a34a898bf9cf7cd29301f76fd5ee6379275
Suelen Salvino <suelensalvinocs@gmail.com>
2021-01-14 23:37:10 -0300

    Models e scripts de criação de tabela

---
+++ database/factories/EstadiosFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Estadios;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EstadiosFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Estadios::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
