273ddf566efc8ec8efb96e82eb63f6032770ab42
Suelen Salvino <suelensalvinocs@gmail.com>
2021-01-22 18:31:58 -0300

    Listagem de jogos e estádios

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @foreach ($user->perfis as $perfil)
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    Jogo:
+                    {{ $perfil->ListaJogos }}
+                    <br>
+                    Estádio:
+                    {{$perfil->ListaEstadios }}
                 </div>
+                @endforeach
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,8 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [HomeController::class, 'dashboard'])->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
